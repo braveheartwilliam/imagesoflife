707447cd35c4a693424fbd225b1605e73c6e3d56
braveheartwilliam <braveheartwilliam@hotmail.com>
2024-01-02 02:45:29 -0500

    a

--- ImagesOfLife/vite.config.js
+++ ImagesOfLife/vite.config.js
@@ -3,6 +3,6 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	base: "/MAGESOFLIFE/",
+	base: "/imagesoflife/",
 	plugins: [svelte()],
 })
