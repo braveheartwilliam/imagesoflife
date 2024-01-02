e5e875280be11921c42678d304ae2f5fdd801353
braveheartwilliam <braveheartwilliam@hotmail.com>
2024-01-02 18:04:06 -0500

    1

--- ImagesOfLife/src/App.svelte
+++ ImagesOfLife/src/App.svelte
@@ -1,17 +1,93 @@
 <script>
+	import Breadcrumbs from './lib/Breadcrumbs.svelte';
+
+	const items = [{ text: 'Home', href: '/' }];
 </script>
 
 <main>
-	<div class="HomePage">Images of Life</div>
+	<div class="HomePage">
+		Images of Life xxx
+		<div class="grid_container">
+			<div class="home_header">home_header <Breadcrumbs {items} /></div>
+			<Breadcrumbs {items} />
+			<div class="home_left_navigation">
+				left_navigation
+				<div class="home_nav">
+					What do you want to see?
+					<nav class="home_left_nav">
+						<li class="nav_list">
+							<a href="./src/pages/Places.svelte">Places</a>
+						</li>
+						<li class="nav_list">Trips</li>
+						<li class="nav_list">Pictures</li>
+						<li class="nav_list">People</li>
+						<li class="nav_list">Me</li>
+					</nav>
+				</div>
+			</div>
+			<div class="home_body">home_body</div>
+			<div class="home_right_navigation">right_navigation</div>
+			<div class="home_footer">home_footer</div>
+		</div>
+	</div>
 </main>
 
 <style>
 	.HomePage {
-		width: 95vw;
+		width: 100vw;
+
 		background-color: antiquewhite;
+	}
+	.grid_container {
 		display: grid;
-		grid-template-columns: 20vw repeat(8, 10vw) 20dvw;
-		grid-template-rows: 15rem repeat(6, 10rem) 15rem;
+		grid-template-columns: 15vw repeat(8, 10vw) 15dvw;
+		grid-template-rows: 15rem repeat(6, 15rem) 15rem;
+	}
+	.home_left_navigation {
+		background-color: aquamarine;
+		font-size: 25px;
+		color: red;
+		grid-column: 1/1;
+		grid-row: 2/8;
+	}
+	.home_nav {
+		font-size: 25px;
+		color: red;
+		width: 8rem;
+		margin: auto;
+		display: flex;
+		flex-direction: column;
+		text-align: left;
+	}
+
+	.home_right_navigation {
+		background-color: rgb(49, 108, 177);
+		font-size: 25px;
+		color: red;
+		grid-column: 10/11;
+		grid-row: 2/8;
+	}
+	.home_header {
+		background-color: rgb(230, 227, 100);
+		font-size: 25px;
+		color: red;
+		grid-column: 1/11;
+		grid-row: 1/1;
+		color: black;
+	}
+	.home_body {
+		background-color: rgb(236, 208, 81);
+		font-size: 25px;
+		color: red;
+		grid-column: 2/10;
+		grid-row: 2/8;
+	}
+	.home_footer {
+		background-color: rgb(240, 169, 240);
+		font-size: 25px;
+		color: red;
+		grid-column: 1/11;
+		grid-row: 8/8;
 	}
 	.logo:hover {
 		filter: drop-shadow(0 0 2em #646cffaa);
---
+++ ImagesOfLife/src/lib/Breadcrumbs.svelte
@@ -0,0 +1,33 @@
+<script>
+	export let items = [];
+</script>
+
+<div style="font-size: large; color: blueviolet;">in breadcrumbs</div>
+<nav aria-label="breadcrumb">
+	<ul class="breadcrumb">
+		{#each items as item, i}
+			<li class="breadcrumb-item">
+				{#if item.href}
+					<a href={item.href}>{item.text}</a>
+				{:else}
+					<span>{item.text} Crumb Text</span>
+				{/if}
+			</li>
+			{#if i !== items.length - 1}
+				<li class="breadcrumb-item separator">..</li>
+			{/if}
+		{/each}
+	</ul>
+</nav>
+
+<style>
+	.breadcrumb {
+		display: flex;
+	}
+	.breadcrumb-item {
+		color: black;
+		font-size: medium;
+	}
+	breadcrumb-item separator {
+	}
+</style>
